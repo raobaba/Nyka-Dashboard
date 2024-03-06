fe91a42a23bf4b2219006b8a1c19b8c76058e236
Rajan Kumar <raorajan9576@gmail.com>
2024-03-06 11:47:10 +0530

    Dashboard route created

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,19 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Analytics from './Analytics';
-import Sidebar from './Sidebar';
+import { RxDashboard } from 'react-icons/rx';
+import { SiGoogleanalytics } from 'react-icons/si';
+import { CiLogout } from 'react-icons/ci';
 
 function Dashboard() {
+  const [activeContent, setActiveContent] = useState('dashboard');
+
+  const handleButtonClick = (content) => {
+    setActiveContent(content);
+  };
+
   return (
-    <div className='w-full border border-red-600 h-screen flex justify-between'>
-      <div className='border w-2/12'>
-       
+    <div className='w-full border border-red-600 h-screen flex'>
+      {/* Sidebar */}
+      <div className='h-60 w-1/6 p-4 flex flex-col justify-between'>
+        <h2 className='text-2xl font-bold'>Nyka Dashboard</h2>
+        <div className='flex flex-col'>
+          <button
+            className={`flex items-center p-2 mb-3 rounded ${
+              activeContent === 'dashboard' ? 'bg-blue-500 text-white' : ''
+            }`}
+            onClick={() => handleButtonClick('dashboard')}
+          >
+            <RxDashboard className='mr-2' />
+            Dashboard
+          </button>
+          <button
+            className={`flex items-center mb-3 p-2 rounded ${
+              activeContent === 'analytics' ? 'bg-blue-500 text-white' : ''
+            }`}
+            onClick={() => handleButtonClick('analytics')}
+          >
+            <SiGoogleanalytics className='mr-2' />
+            <Analytics/>
+          </button>
+          <button className='flex items-center hover:bg-blue-500 p-2 rounded'>
+            <CiLogout className='mr-2' />
+            Logout
+          </button>
+        </div>
       </div>
-      <div className='border w-10/12'>
-      <Sidebar/>
-      <Analytics/>
+
+      {/* Main Content */}
+      <div className='border w-5/6 p-4'>
+        {activeContent === 'dashboard' && (
+          <div>
+            {/* Content for the Dashboard */}
+            <h2>Dashboard Content</h2>
+          </div>
+        )}
+        {activeContent === 'analytics' && (
+          <div>
+            {/* Render the Analytics component */}
+            <Analytics />
+          </div>
+        )}
       </div>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
