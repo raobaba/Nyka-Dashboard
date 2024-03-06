5de0d0974c6de19e5eb37f5b9c1babead54202fd
Rajan Kumar <raorajan9576@gmail.com>
2024-03-06 18:05:24 +0530

    prevent duplicate email

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,10 +1,15 @@
-const User = require('../models/user.model');
+const User = require("../models/user.model");
 const cloudinary = require("cloudinary");
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
 const createUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
 
     const myCloud = await cloudinary.uploader.upload(
       req.files.avatar.tempFilePath,
@@ -26,45 +31,49 @@ const createUser = async (req, res) => {
 
     await newUser.save();
 
-    res.status(201).json({ message: 'User created successfully', user: newUser });
+    res
+      .status(201)
+      .json({ message: "User created successfully", user: newUser });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
 const loginUser = async (req, res) => {
-    try {
-      const { email, password } = req.body;
-      const user = await User.findOne({ email });
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
 
-      if (!user) {
-        return res.status(401).json({ message: 'Please check your email' });
+    if (!user) {
+      return res.status(401).json({ message: "Please check your email" });
     }
 
-      if (user.password!==password) {
-        return res.status(401).json({ message: 'Please check your password' });
-      }
-      
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRE });
-
-      res.status(200).json({ message: 'Login successful',user, token });
-    } catch (error) {
-      console.error(error);
-
-      res.status(500).json({ message: 'Internal server error' });
+    if (user.password !== password) {
+      return res.status(401).json({ message: "Please check your password" });
     }
-  };
-  
-  const logoutUser = (req, res) => {
-    res.cookie("token", null, {
-      expires: new Date(Date.now()),
-      httpOnly: true,
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRE,
     });
-    res.status(200).json({
-      success: true,
-      message: "Logged Out",
-    });
-  };
-  
-  module.exports = { createUser, loginUser, logoutUser };
+
+    res.status(200).json({ message: "Login successful", user, token });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const logoutUser = (req, res) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    success: true,
+    message: "Logged Out",
+  });
+};
+
+module.exports = { createUser, loginUser, logoutUser };
